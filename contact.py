42c31a68b809908f1a96192bf9ec7a6e764521da
Elena Malysheva <kurbatovaei@gmail.com>
2016-07-12 18:54:50 +0300

    test_add_contact refactor similar to test_add_group
    1. Parametrize the first test for adding not empty contact.
    2. Add the second test for adding empty contact.
    3. Segregate contact params to a separate file.

---
+++ contact.py
@@ -0,0 +1,9 @@
+class Contact:
+    def __init__(self, firstname, lastname, address, home_phone, mobile_phone, work_phone, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.email = email
