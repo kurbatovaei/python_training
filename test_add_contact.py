cb20f073f93f7b220ef32e03d69ad85ed298457c
Elena Malysheva <kurbatovaei@gmail.com>
2016-07-17 13:47:18 +0300

    initial moving tests from unittest to pytest

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,38 +1,27 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from contact import Contact
 from application import Application
 
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalazer(fixture.destroy)
+    return fixture
 
 
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+def test_add_contact(app):
+    app.login("admin", "secret")
+    app.create_contact(Contact(firstname="Barack", lastname="Obama",
+                               address="White House, 1600 Pennsylvania Avenue NW, Washington, D.C.",
+                               home_phone="202-456-1111", mobile_phone="202-456-1414",
+                               work_phone="202-456-2121", email="mr.president@white.house"))
+    app.logout()
 
-    def test_add_contact(self):
-        self.app.login("admin", "secret")
-        self.app.create_contact(Contact(firstname="Barack", lastname="Obama",
-                                    address="White House, 1600 Pennsylvania Avenue NW, Washington, D.C.",
-                                    home_phone="202-456-1111", mobile_phone="202-456-1414",
-                                    work_phone="202-456-2121", email="mr.president@white.house"))
-        self.app.logout()
 
-    def test_add_empty_contact(self):
-        self.app.login("admin", "secret")
-        self.app.create_contact(Contact(firstname="", lastname="", address="", home_phone="", mobile_phone="",
-                                    work_phone="", email=""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_add_empty_contact(app):
+    app.login("admin", "secret")
+    app.create_contact(Contact(firstname="", lastname="", address="", home_phone="", mobile_phone="",
+                               work_phone="", email=""))
+    app.logout()
