4422fa54d03b9e17c4f9c656b7cc8d97f40b7d6b
Elena Malysheva <kurbatovaei@gmail.com>
2016-07-17 14:00:05 +0300

    moving tests from unittest to pytest with fixture as param

--- test_add_contact.py
+++ test_add_contact.py
@@ -7,7 +7,7 @@ from application import Application
 @pytest.fixture
 def app(request):
     fixture = Application()
-    request.addfinalazer(fixture.destroy)
+    request.addfinalizer(fixture.destroy)
     return fixture
 
 
@@ -15,13 +15,13 @@ def test_add_contact(app):
     app.login("admin", "secret")
     app.create_contact(Contact(firstname="Barack", lastname="Obama",
                                address="White House, 1600 Pennsylvania Avenue NW, Washington, D.C.",
-                               home_phone="202-456-1111", mobile_phone="202-456-1414",
-                               work_phone="202-456-2121", email="mr.president@white.house"))
+                               home_phone="202-456-1111", mobile_phone="202-456-1414", work_phone="202-456-2121",
+                               email="mr.president@white.house"))
     app.logout()
 
 
 def test_add_empty_contact(app):
     app.login("admin", "secret")
-    app.create_contact(Contact(firstname="", lastname="", address="", home_phone="", mobile_phone="",
-                               work_phone="", email=""))
+    app.create_contact(Contact(firstname="", lastname="", address="", home_phone="", mobile_phone="", work_phone="",
+                               email=""))
     app.logout()
--- test_add_group.py
+++ test_add_group.py
@@ -7,7 +7,7 @@ from application import Application
 @pytest.fixture
 def app(request):
     fixture = Application()
-    request.addfinalazer(fixture.destroy)
+    request.addfinalizer(fixture.destroy)
     return fixture
 
 
