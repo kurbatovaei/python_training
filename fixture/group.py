85a6998dc7b6d940cc29ccd983859d617553cf34
Elena Malysheva <kurbatovaei@gmail.com>
2016-07-18 20:45:45 +0300

    add test for group editing

--- fixture/group.py
+++ fixture/group.py
@@ -7,12 +7,8 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
-    def create(self, group):
+    def fill_group_form(self, group):
         wd = self.app.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -24,6 +20,13 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        self.fill_group_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -35,6 +38,19 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         # submit deletion
         wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+    def edit_first_group(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # init group editing
+        wd.find_element_by_name("edit").click()
+        # fill group form with new values
+        self.fill_group_form(group)
+        # submit update
+        wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
     def return_to_groups_page(self):
