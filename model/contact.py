38f3e3361bf8de4e8f73e03308e413fba1f554d1
Elena Malysheva <kurbatovaei@gmail.com>
2016-08-02 17:09:57 +0300

    verify test_add_contact by comparing contact lists

--- model/contact.py
+++ model/contact.py
@@ -1,3 +1,6 @@
+from sys import maxsize
+
+
 class Contact:
     def __init__(self, firstname=None, lastname=None, address=None, home_phone=None, mobile_phone=None, work_phone=None,
                  email=None, id=None):
@@ -14,4 +17,11 @@ class Contact:
         return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
 
     def __eq__(self, other):
-        return self.id == other.id and self.firstname == other.firstname and self.lastname == other.lastname
+        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname \
+               and self.lastname == other.lastname
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
