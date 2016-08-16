5b91f90a3c0a427dbec99e52af6fcd4d6b848cd2
Elena Malysheva <kurbatovaei@gmail.com>
2016-08-16 14:49:49 +0300

    load config params from json file

--- fixture/application.py
+++ fixture/application.py
@@ -6,7 +6,8 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self, browser, base_url, login, password):
+    #def __init__(self, browser, base_url, login, password):
+    def __init__(self, browser, base_url):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -20,8 +21,8 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
         self.base_url = base_url
-        self.login = login
-        self.password = password
+        #self.login = login
+        #self.password = password
 
     def is_valid(self):
         try:
