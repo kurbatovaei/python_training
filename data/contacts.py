166400d1deef0e06deccae003e4bc28c8b99cff3
Elena Malysheva <kurbatovaei@gmail.com>
2016-08-21 16:54:31 +0300

    dynamic tests generation

---
+++ data/contacts.py
@@ -0,0 +1,8 @@
+from model.contact import Contact
+
+testdata = \
+    [Contact(firstname="", lastname="", address="", home_phone="", mobile_phone="", work_phone="", email1="")] \
+    + \
+    [Contact(firstname="Barack", lastname="Obama", address="White House, 1600 Pennsylvania Avenue NW, Washington, D.C.",
+             home_phone="202-456-1111", mobile_phone="202-456-1414", work_phone="202-456-2121",
+             email1="mr.president@white.house")]\
