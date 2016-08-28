1eb8bb07d0cbb3032038e76cd55af6584d7e1ab7
Elena Malysheva <kurbatovaei@gmail.com>
2016-08-28 19:51:40 +0300

    trying orm

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,27 +1,39 @@
 # import mysql.connector
 import pymysql.cursors
 from fixture.db import DbFixture
+from fixture.orm import ORMFixture
 
-# connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="")
-connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
-
-try:
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for row in cursor.fetchall():
-        print(row)
-finally:
-    connection.close()
+# # connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+# connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+#
+# try:
+#     cursor = connection.cursor()
+#     cursor.execute("select * from group_list")
+#     for row in cursor.fetchall():
+#         print(row)
+# finally:
+#     connection.close()
+#
+# # -----------------
+#
+# db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+#
+# try:
+#     groups = db.get_group_list()
+#     for group in groups:
+#         print(group)
+#     print(len(groups))
+# finally:
+#     db.destroy()
 
 # -----------------
 
-db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    groups = db.get_group_list()
-    for group in groups:
-        print(group)
-    print(len(groups))
+    l = db.get_contact_list()
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    db.destroy()
-
+    pass
